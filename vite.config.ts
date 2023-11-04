02a426527d6035b334fcf057887073ae935af08a
av.litvin <av.litvin73@gmail.com>
2023-11-04 19:27:17 +0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/memory/',
   plugins: [react()],
   test: {
     environment: "jsdom",
